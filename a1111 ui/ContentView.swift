c5ad6286308d0a608aded3565b98ad44f8327272
sus <amilucky8108@gmail.com>
2022-10-17 19:23:10 +0530

    Initial Commit

---
+++ a1111 ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  a1111 ui
+//
+//  Created by Ayan Rajpurohit on 17/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ a1111 ui/a1111_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  a1111_uiApp.swift
+//  a1111 ui
+//
+//  Created by Ayan Rajpurohit on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct a1111_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
